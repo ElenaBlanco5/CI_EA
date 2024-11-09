8aa5f0d774a7db68b32242f0b8f33187d9fde32c
ElenaBlanco5 <eb4431@outlook.es>
2024-11-09 15:58:52 +0100

    hoa

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import cma
+from tqdm import tqdm
 import math
 
 def salomon(x):
@@ -16,7 +16,6 @@ def hosaki(x):
     return (1 - 8 * x[0] + 7 * x[0]**2 - (7/3) * x[0]**3 + (1/4) * x[0]**4) * x[1]**2 * math.exp(-x[1])
 
 def ackley(x):
-    """ Ackley function for optimization problem """
     n = len(x)
     sum_sq = np.sum(x ** 2)
     sum_cos = np.sum(np.cos(2 * np.pi * x))
@@ -36,18 +35,19 @@ for key in ['initial_sol', 'best_sol', 'best_fit', 'iter']:
 # Set initial parameters
 np.random.seed(42)
 n = 2
-sigma0 = 0.2
-f_name = 'salomon'
+sigma0 = 0.5
+f_name = 'hosaki'
 options = cma.CMAOptions()
 options['popsize'] = 4+math.floor(3*math.log(n))
 options['maxiter'] = 100
 options['tolfun'] = 1e-13
-options['bounds'] = [[-5, -5], [5, 5]]
+options['seed'] = 42
+options['bounds'] = [[0, 0], [5, 5]]
 options['maxfevals'] = 1e3*n**2
 
 alg = []
 convergence = []
-for i in range(50):
+for i in tqdm(range(50)):
     x01 = np.random.uniform(options['bounds'][0][0], options['bounds'][1][0])
     x02 = np.random.uniform(options['bounds'][0][1], options['bounds'][1][1])
     x0 = (x01,x02)
@@ -56,7 +56,7 @@ for i in range(50):
     es = cma.CMAEvolutionStrategy(x0, sigma0, options)
 
     # Optimize using CMA-ES
-    es.optimize(salomon)
+    es.optimize(hosaki)
 
     # Get the best solution found
     best_solution = es.result.xbest
@@ -69,8 +69,7 @@ for i in range(50):
     results['best_fit'].append(best_value)
     results['iter'].append(iter)
     alg.append(es)
-
-    if abs(best_value) < 1e-13:
+    if abs(best_value-(-2.3458)) < 1e-4:
         convergence.append(i)
 
 # Print the results
@@ -79,13 +78,13 @@ print(f"Best fitness value: {np.min(results['best_fit'])}")
 best_index = np.argmin(results['best_fit'])
 print(f"Best solution: {results['best_sol'][best_index]}")
 print(f"Initial solution: {results['initial_sol'][best_index]}")
-print(f"Iterations: [{np.min(results['iter'])},{np.max(results['iter'])}]")
+print(f"Iterations: {results['iter'][best_index]}")
 
 # Plot initial guesses
-x = [p[0] for p in results['initial_sol']]
-y = [p[1] for p in results['initial_sol']]
-plt.scatter(x[convergence], y[convergence], color='blue', s=10)  # Scatter plot of points
-plt.scatter(x[], y[])
+conv = [results['initial_sol'][idx] for idx in convergence]
+no_conv = [el for i,el in enumerate(results['initial_sol']) if i not in convergence]
+plt.scatter([p[0] for p in conv], [p[1] for p in conv], color='blue', s=10)
+plt.scatter([p[0] for p in no_conv], [p[1] for p in no_conv], color='red', s=10)
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.savefig(f'plots/{f_name}_initial_guesses.png', format='png')
